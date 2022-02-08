17d88e02ad580f4d402316ba9b02d04dc0c19092
levi-manoel <levimanoel.deob@gmail.com>
2022-02-07 23:31:52 -0300

    aniversário denis

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route  } from 'react-router';
 
-import EasterEgg from './pages/EasterEgg';
 import Home from './pages/Home';
+import EasterEgg from './pages/EasterEgg';
+import NiverDenis from './pages/NiverDenis';
 
 import './App.css';
 
@@ -10,7 +11,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/levi-manoel" element={ <Home /> } />
-        <Route path="/easter" element={ <EasterEgg /> } />
+        <Route path="/levi-manoel/easter" element={ <EasterEgg /> } />
+        <Route path="/levi-manoel/denis" element={ <NiverDenis /> } />
       </Routes>
     </div>
   );
---
+++ src/pages/NiverDenis.js
@@ -0,0 +1,124 @@
+import React from 'react';
+
+import './styles/denis.css';
+
+function NiverDenis() {
+  return (
+    <>
+      <header>
+        <h1>Olá peteu,</h1>
+        <em>Se você não for o peteu, pode sair</em>
+        <em>"Ah mas foi ela que me mandou aqui"</em>
+        <em>Não perguntei, vaza</em>
+        <em>"Ah, mas vi no código da página que tem essa rota"</em>
+        <em>Vou nem responder<span>VAZA</span></em>
+        <em>"Ah, mas sou seu pai/mãe/sogro/sogra"</em>
+        <em>Pode ficar a vontade, que um cafézinho?</em>
+      </header>
+
+      <main>
+        <section>
+          <h2>
+            "Hoje é o dia do seu aniversário, que dia feliz que dia abençoado."
+            <em>MENEGUEL, Xuxa</em>
+          <img src="https://i.imgur.com/vIGnD6L.jpg" alt="hepi bardei"/>
+          </h2>
+        </section>
+
+        <div id="aviso">
+          <p>A partir daqui as informações são sensíveis e confidenciais, prossiga por conta e risco</p>
+        </div>
+
+        <section>
+          <h2>
+            Apresentando a meliante
+          </h2>
+          <p>Segue abaixo um 360 do alvo:</p>
+          <img src="https://i.imgur.com/o6z1lY3.jpg" alt="360 da gatah 1"/>
+          <img src="https://i.imgur.com/KhoC5SQ.jpg" alt="360 da gatah 2"/>
+          <img src="https://i.imgur.com/5XIz2af.jpg" alt="360 da gatah 3"/>
+          <img src="https://i.imgur.com/9EdeC6M.jpg" alt="360 da gatah 4"/>
+
+          <div>
+          <h3>
+            Características:
+          </h3>
+          <ul>
+            <li>Olhos: bonitos</li>
+            <li>Cabelo: bonito</li>
+            <li>Altura: piquitotinha</li>
+            <li>Tom de pele: bonito</li>
+            <li>Voz: bonita</li>
+          </ul>
+          </div>
+        </section>
+
+        <section className="sectionIm">
+          <h2>
+            Disfarces
+          </h2>
+          <p>Como uma bandida experiente, ela já utilizou vários, aqui temos os mais conhecidos:</p>
+          <h3>Disfarce triste</h3>
+          <p>Como uma boa fugitiva, ela também é uma ótima atora, muitas vezes utilizando esse disfarce para "lubriar" suas vítimas.</p>
+          <img src="https://i.imgur.com/BNw2jbD.jpg" alt="disfarce triste 1"/>
+          <img src="https://i.imgur.com/cAbjRr5.jpg" alt="disfarce triste 2"/>
+          <img src="https://i.imgur.com/AoRYYtn.jpg" alt="disfarce triste 3"/>
+          <img src="https://i.imgur.com/JRBO37R.jpg" alt="disfarce triste 4"/>
+          <img src="https://i.imgur.com/3oPseeG.jpg" alt="disfarce triste 5"/>
+
+          <h3>Disfarce carismático</h3>
+          <p>Outro ponto forte de bons violantes da lei é seu carisma, esse é o disfarce que nossa bandida usa para ganhar a simpatia da grande massa.</p>
+          <img src="https://i.imgur.com/t7Fl0u7.jpg" alt="disfarce carismático 1"/>
+          <img src="https://i.imgur.com/1I85R1c.jpg" alt="disfarce carismático 2"/>
+          <img src="https://i.imgur.com/cwLj3xx.jpg" alt="disfarce carismático 3"/>
+        </section>
+
+        <section>
+          <h2>Informações sobre a quadrilha Sonecas Imposto</h2>
+          <p>Esse nome foi dado ao grupo já que gostam de uma sonequinha e sonegam imposto.</p>
+          <img src="https://i.imgur.com/kHZ2a5f.jpg" alt="soneca 1"/>
+
+          <h2>Integrantes da quadrilha</h2>
+          <p>Temos conhecimento, até o atual momento, de 3 integrantes.</p>
+          <h3>Soneca, a líder</h3>
+          <p>Informações sobre ela já estão na página acima</p>
+          <img src="https://i.imgur.com/nJ4mFKY.jpg" alt="lider" />
+          <h3>Frango, a galinha</h3>
+          <p>A melhor amiga da líder, boatos dizem que são gêmeas, comparação abaixo:</p>
+          <img src="https://i.imgur.com/OiBk6So.jpg" alt="galinha" />
+          <img src="https://i.imgur.com/c2ajhGB.jpg" alt="lider galinha" />
+          <h3>Besta, o besta</h3>
+          <p>O integrante de nível mais baixo na quadrilha, rolam Informações que ele é o felizardo que tem um caso amoroso com a líder</p>
+          <img src="https://i.imgur.com/vxv2bpS.jpg" alt="besta lider" />
+        </section>
+
+        <section className="sectionIm">
+          <h2>Informações adicionais</h2>
+          <h3>Xena, a prencesa guerrera</h3>
+          <p>Um dos muitos nomes por qual soneca é conhecida, ganhou esse apelido após uma combate sangrento com um membro da gangue rival. Abaixo imagens do duelo:</p>
+          <img src="https://i.imgur.com/tarti0Q.jpg" alt="duelo 1" />
+          <p>Primeiro ela avista a vítima</p>
+          <img src="https://i.imgur.com/3AyazVK.jpg" alt="duelo 2" />
+          <p>Depois lança sobre ele um olhar que dá arrepios</p>
+          <img src="https://i.imgur.com/CYz7WIT.jpg" alt="duelo 3" />
+          <p>E por último após deixar a vítima incapacitada, ri de seu sofrimento por tempo indeterminado</p>
+
+          <h3>Centro de treinamento</h3>
+          <p>Após atingir o nível de mais procurada, fundou um centro para treinamento de novos integrantes de sua quadrilha</p>
+          <p>Na imagem abaixo podemos vê-lá infiltrada em uma das aulas para determinar a qualidade de seus novos servos</p>
+          <img src="https://imgur.com/muA8Q00" alt="lider no CT"/>
+
+          <h3>Cuidado ao se aproximar</h3>
+          <p>A história por trás da imagem abaixo é deveras triste. Um de nossos espiões foi falar com soneca para coletar informações, mas ao fazer isso no seu horário de refeição mal sabia ele que isso seria a última coisa que ele veria.</p>
+          <img src="https://i.imgur.com/iSCMwR7.jpg" alt="F"/>
+        </section>
+      </main>
+
+      <footer>
+        Caso tenha mais informações, entre em contato pelo número +55 (11) 9 4002-8922
+      </footer>
+    </>
+  );
+}
+
+export default NiverDenis;
