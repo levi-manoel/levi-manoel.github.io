d72b5d23fa9915406d09d83878392c162482f0f5
levi-manoel <levimanoel.deob@gmail.com>
2022-05-10 09:35:11 -0300

    adds links to the home cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/Card.css';
 
 function Card(props) {
-  const { title, text } = props;
-  return (
-    <div className='card'>
-      <h3>{ title }</h3>
-      <hr />
-      <p>{ text }</p>
-    </div>
+  const { title, text, linkTo, url } = props;
+
+  const returnCard = url ? (
+    <a href={ url }>
+      <div className='card'>
+        <h3>{ title }</h3>
+        sale
+        <hr />
+        <p>{ text }</p>
+      </div>
+    </a>
+  ) : (
+    <Link to={ linkTo }>
+      <div className='card'>
+        <h3>{ title }</h3>
+        <hr />
+        <p>{ text }</p>
+      </div>
+    </Link>
   );
+  
+  return returnCard;
 }
 
 export default Card;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,10 +10,10 @@ function Home() {
       <hr />
       <main>
         <div className='grid'>
-          <Card title="Sobre mim" text="Veja minhas habilidades e um pouco da caminhada até aqui" />
-          <Card title="Projetos" text="Veja meus projetos de orgulho, tanto ele rodando como seu repositório no github" />
-          <Card title="Github" text="Veja minha conta no Github" />
-          <Card title="LinkedIn" text="Veja minha conta no LinkedIn" />
+          <Card title="Sobre mim" text="Veja minhas habilidades e um pouco da caminhada até aqui" linkTo="/sobre" />
+          <Card title="Projetos" text="Veja meus projetos de orgulho, tanto ele rodando como seu repositório no github" linkTo="/projetos"/>
+          <Card title="Github" text="Veja minha conta no Github" url="www.github.com/levi-manoel" />
+          <Card title="LinkedIn" text="Veja minha conta no LinkedIn" url="www.linkedin.com/in/levi-manoel" />
         </div>
       </main>
     </>
