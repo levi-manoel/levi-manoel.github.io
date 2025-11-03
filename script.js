beebbe670bad6205da7aa51b1dd4d3d4cbb39682
levi-manoel <levimanoel.deob@gmail.com>
2025-11-03 14:04:22 -0300

    refact

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerBtn = document.getElementById("hamburger-btn");
-const navList = document.getElementById("nav-list");
-
-hamburgerBtn.addEventListener("click", () => {
-    navList.classList.toggle("active");
-});
